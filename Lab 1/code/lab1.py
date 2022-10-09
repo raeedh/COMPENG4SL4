b0a999ea404957219722c3abcb978e64ce74fc5c
Raeed Hassan <32547662+raeedh@users.noreply.github.com>
2022-10-09 19:29:55 -0400

    finish plotting

--- Lab 1/code/lab1.py
+++ Lab 1/code/lab1.py
@@ -49,6 +49,18 @@ def calc_error(x, t, N, predictor):
 		cum_error += (t[i] - cum)**2
 	return cum_error / N
 
+def calc_error_regularization(x, t, N, lmbda, predictor):
+	cum_error = 0
+	for i in range(N):
+		cum = 0
+		for n, d in enumerate(predictor):
+			cum += d*x[i]**n
+		cum_error += (t[i] - cum)**2
+	regularized_cost = cum_error / N
+	for val in predictor:
+		regularized_cost += lmbda*(val**2)
+	return regularized_cost 
+
 # Feature standardization
 def feature_standardization(X_train, X_valid, M):
 	XX_train = np.tile(X_train.reshape(-1,1),M)
@@ -98,6 +110,7 @@ def main():
 	training_error = []
 	for i in range(len(w)):
 		training_error.append(calc_error(X_train, t_train, 10, w[i]))
+	# Print results
 	print("The training error for all predictors was calculated:")
 	for i in range(10):
 		print("M = " + str(i) + ", " + str(training_error[i]))
@@ -108,6 +121,7 @@ def main():
 	validation_error = []
 	for i in range(len(w)):
 		validation_error.append(calc_error(X_valid, t_valid, 10, w[i]))
+	# Print results
 	print("The validation error for all predictors was calculated:")
 	for i in range(10):
 		print("M = " + str(i) + ", " + str(validation_error[i]))
@@ -115,7 +129,7 @@ def main():
 	print("\n")
 
 	# Generate predictors for M = 9 with regularization for various lambda
-	lambdas = [math.exp(-5), math.exp(-6), math.exp(-7), math.exp(-8), math.exp(-9), math.exp(-10), math.exp(-11), math.exp(-12), math.exp(-13), math.exp(-14), math.exp(-15), math.exp(-16), math.exp(-17), math.exp(-18), math.exp(-19), math.exp(-20), math.exp(-21), math.exp(-22), math.exp(-23), math.exp(-24), math.exp(-25), math.exp(-30), math.exp(-40), math.exp(-50)]
+	lambdas = [math.exp(0), math.exp(-1), math.exp(-2), math.exp(-3), math.exp(-4), math.exp(-5), math.exp(-6), math.exp(-7), math.exp(-8), math.exp(-9), math.exp(-10), math.exp(-11), math.exp(-12), math.exp(-13), math.exp(-14), math.exp(-15), math.exp(-16), math.exp(-17), math.exp(-18), math.exp(-19), math.exp(-20), math.exp(-21), math.exp(-22), math.exp(-23), math.exp(-24), math.exp(-25), math.exp(-30), math.exp(-40), math.exp(-50)]
 	w9_reg = []
 	for val in lambdas: # use additional argument λ for least_square_regression to enable regularization (default value is 0 if no argument passed in)
 		w9_reg.append(least_square_regression(X_train, t_train, 9, val))
@@ -125,7 +139,8 @@ def main():
 	# Calculate training error for all predictors for M = 9 with regularization
 	training_error_reg = []
 	for i in range(len(w9_reg)):
-		training_error_reg.append(calc_error(X_train, t_train, 10, w9_reg[i]))
+		training_error_reg.append(calc_error_regularization(X_train, t_train, 10, lambdas[i], w9_reg[i]))
+	# Print results
 	print("The training error for various \u03BB was calculated:")
 	for i in range(len(training_error_reg)):
 		print("ln \u03BB = " + str(math.log(lambdas[i])) + ", " + str(training_error_reg[i]))
@@ -135,11 +150,21 @@ def main():
 	# Calculate validation error for all predictors for M = 9 with regularization
 	validation_error_reg = []
 	for i in range(len(w9_reg)):
-		validation_error_reg.append(calc_error(X_valid, t_valid, 10, w9_reg[i]))
+		validation_error_reg.append(calc_error_regularization(X_valid, t_valid, 10, lambdas[i], w9_reg[i]))
+	# Print results
 	print("The validation error for various \u03BB was calculated:")
 	for i in range(len(validation_error_reg)):
 		print("ln \u03BB = " + str(math.log(lambdas[i])) + ", " + str(validation_error_reg[i]))
 	print("The lowest validation error with regularization is " + str(min(validation_error_reg)) + " when ln \u03BB = " + str(math.log(lambdas[validation_error_reg.index(min(validation_error_reg))])) + ".")
+	print("The predictor coefficients for this linear regression is:")
+	print(w9_reg[validation_error_reg.index(min(validation_error_reg))])
+	print("\n")
+
+	print("We observe we reduce overfitting when \u03BB is between -10 and -25, an example is when ln \u03BB = -24 (the lowest validation error), for which the predictor coefficients are:")
+	print(w9_reg[lambdas.index(math.exp(-24))])
+
+	print("We observe the plot is underfitted when \u03BB is too high, an example is when ln \u03BB = 0, for which the predictor coefficients are:")
+	print(w9_reg[lambdas.index(math.exp(0))])
 
 	XX_train, XX_valid = feature_standardization(X_train, X_valid, 9)
 
